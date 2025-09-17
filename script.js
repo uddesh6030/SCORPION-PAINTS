328a1e84049d12f44cac3ae351eae6593bcec2aa
uddesh6030 <uddeshmore6030@gmail.com>
2025-09-17 22:46:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Placeholder for JavaScript functionality
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+    
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        alert("Your message has been sent!");
+    });
+});
